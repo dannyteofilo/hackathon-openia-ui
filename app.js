66d5176c5529dae3a88ac52081ca0b809dc1590e
dannyteofilo <danyel.285309@gmail.com>
2023-07-07 13:43:56 -0600

    feat: config dotenv

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
 const authRoutes = require('./src/routes/auth.routes.js');
+const { protectRoute } = require('./src/middlewares/protectRoute.js');
+require('dotenv').config();
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
@@ -10,16 +13,20 @@ app.set('view engine', 'ejs');
 app.set('views', './src/views');
 
 app.get('/', (req, res) => {
-  res.render('home');
+    res.render('home');
 });
 
 app.get('/login', (req, res) => {
-  res.render('login');
+    res.render('login');
+});
+
+app.get('/ia', protectRoute, (req, res) => {
+    res.render('ia');
 });
 
 app.use('/auth', authRoutes);
 
-const port = 3000;
+
 app.listen(port, () => {
-  console.log(`Servidor iniciado en http://localhost:${port}`);
+    console.log(`Servidor iniciado en http://localhost:${port}`);
 });
